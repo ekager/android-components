8adf6c6b5a3f753f009eced69baad7aec305ecbc
Emily Kager <ekager@mozilla.com>
2019-12-10 13:29:52 -0800

    For #TODO: add LoginStorageDelegate#onLoginSave

--- components/browser/engine-gecko-nightly/src/main/java/mozilla/components/browser/engine/gecko/autofill/LoginStorageDelegate.kt
+++ components/browser/engine-gecko-nightly/src/main/java/mozilla/components/browser/engine/gecko/autofill/LoginStorageDelegate.kt
@@ -6,6 +6,7 @@ package mozilla.components.browser.engine.gecko.autofill
 
 import mozilla.appservices.logins.LoginsStorage
 import mozilla.components.lib.dataprotect.SecureAbove22Preferences
+import mozilla.components.service.sync.logins.ServerPassword
 import org.mozilla.geckoview.GeckoResult
 
 // Temporary Data Class before lands in GV
@@ -28,7 +29,7 @@ internal interface LoginDelegate {
 
     // Request logins for the given domain.
     // @Fenix: return AsyncLoginsStorage.getByHostname(domain).
-    fun onLoginRequest(domain: String): GeckoResult<Array<Login>>
+    fun onFetchLogins(domain: String): GeckoResult<Array<Login>>
 
     // Request to save or update the given login.
     // The hint should help determining the appropriate user prompting
@@ -37,7 +38,7 @@ internal interface LoginDelegate {
     // determine whether to show a Save or Update button on the
     // doorhanger, taking into account un/pw edits in the doorhanger.
     // When the user confirms the save/update,
-    fun onLoginSave(login: Login, hint: Int)
+    fun onLoginSave(login: Login)
 }
 
 /**
@@ -59,7 +60,7 @@ class LoginStorageDelegate(
         }
     }
 
-    override fun onLoginRequest(domain: String): GeckoResult<Array<Login>> {
+    override fun onFetchLogins(domain: String): GeckoResult<Array<Login>> {
         val passwordsKey =
             keyStore.getString(PASSWORDS_KEY) ?: return GeckoResult.fromValue(arrayOf())
         loginStorage.ensureUnlocked(passwordsKey).also {
@@ -71,11 +72,44 @@ class LoginStorageDelegate(
         }
     }
 
-    override fun onLoginSave(login: Login, hint: Int) {
+    // Request to save or update the given login.
+    override fun onLoginSave(login: Login) {
         val passwordsKey = keyStore.getString(PASSWORDS_KEY) ?: return
         loginStorage.ensureUnlocked(passwordsKey).also {
-            // TODO use hints and ensureValid to help determine whether to show update/save prompt
-            // TODO should we use prompt feature / notify Observers to show bottom dialog prompt for logins?
+            val guid = login.guid
+            val serverPassword = guid?.let { loginStorage.get(it) }
+
+            if (guid != null && serverPassword != null) {
+                infix fun String?.orUseExisting(other: String?) = if (this?.isNotEmpty() == true) this else other
+                infix fun String?.orUseExisting(other: String) = if (this?.isNotEmpty() == true) this else other
+
+                val hostname = login.origin orUseExisting serverPassword.hostname
+                val username = login.username orUseExisting serverPassword.username
+                val password = login.password orUseExisting serverPassword.password
+                val httpRealm = login.httpRealm orUseExisting serverPassword.httpRealm
+                val formSubmitUrl = login.formActionOrigin orUseExisting serverPassword.formSubmitURL
+
+                loginStorage.update(
+                    serverPassword.copy(
+                        hostname = hostname,
+                        username = username,
+                        password = password,
+                        httpRealm = httpRealm,
+                        formSubmitURL = formSubmitUrl
+                    )
+                )
+            } else {
+                loginStorage.add(
+                    ServerPassword(
+                        id = "", // TODO ask if this is right? Pass empty string if we don't have it?  If so, ask for it to be null
+                        username = login.username,
+                        password = login.password ?: "",
+                        hostname = login.origin ?: "",
+                        formSubmitURL = login.formActionOrigin,
+                        httpRealm = login.httpRealm
+                    )
+                )
+            }
         }.also {
             loginStorage.lock()
         }
